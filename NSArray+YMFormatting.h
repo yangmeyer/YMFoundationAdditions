7b187853d7ede029443d81b326610d5e7c6cef2f
Yang Meyer <mail@yangmeyer.de>
2012-08-14 16:27:36 -0400

    Add NSArray+YMFormatting category with -formattedListOfElementsUsingLinePrefix:

---
+++ NSArray+YMFormatting.h
@@ -0,0 +1,11 @@
+
+//  Created by YangMeyer on 14.08.12.
+//  Copyright (c) 2012 zenplaya Inc. All rights reserved.
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (YMFormatting)
+
+- (NSString*) formattedListOfElementsUsingLinePrefix:(NSString*)prefix;
+
+@end
---
+++ NSArray+YMFormatting.m
@@ -0,0 +1,15 @@
+
+//  Created by YangMeyer on 14.08.12.
+//  Copyright (c) 2012 zenplaya Inc. All rights reserved.
+
+#import "NSArray+YMFormatting.h"
+
+@implementation NSArray (YMFormatting)
+
+- (NSString*) formattedListOfElementsUsingLinePrefix:(NSString*)prefix {
+	return ([self count]
+			? [NSString stringWithFormat:@"%@%@\n", prefix, [self componentsJoinedByString:[NSString stringWithFormat:@"\n%@", prefix]]]
+			: @"");
+}
+
+@end
