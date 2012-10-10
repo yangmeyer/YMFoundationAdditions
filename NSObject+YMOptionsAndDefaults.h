5ef28cb5cefef64e740cfdd73cd19c343d108ead
Yang Meyer <mail@yangmeyer.de>
2012-10-10 15:55:42 +0200

    Add method documentation.

--- NSObject+YMOptionsAndDefaults.h
+++ NSObject+YMOptionsAndDefaults.h
@@ -6,9 +6,18 @@
 
 @interface NSObject (YMOptionsAndDefaults)
 
+/**
+ Idempotent - calling multiple times simply overwrites previous values.
+ @param	options 	optional, may be nil
+ @param	defaults	required
+ */
 - (void)ym_registerOptions:(NSDictionary *)options
 				  defaults:(NSDictionary *)defaults;
 
+/**
+ @param	optionKey	required
+ @return the value from the options dictionary if it was set, or the default value otherwise.
+ */
 - (id)ym_optionOrDefaultForKey:(NSString*)optionKey;
 
 @end
